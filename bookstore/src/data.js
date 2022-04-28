d8f2eba11669849c23461108ab9e32317ff42a77
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2022-04-28 20:52:04 +0530

    sample data change

--- bookstore/src/data.js
+++ bookstore/src/data.js
@@ -17,8 +17,8 @@ export const booksdata = [
     },
     {
         id:2,
-        title: "The Hunger Games",
-        author: "Suzanne Collins",
+        title: "Harry Potter and the Order of the Phoenix",
+        author: "SuzannJ.K. Rowling, Mary GrandPré (Illustrator)",
         rating: 4.33,
         description: "WINNING MEANS FAME AND FORTUNE.LOSING MEANS CERTAIN DEATH.THE HUNGER GAMES HAVE BEGUN. . . .In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and once girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.Sixteen-year-old Katniss Everdeen regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love.",
         isbn: "9780439023481",
@@ -28,13 +28,13 @@ export const booksdata = [
         publisher: "Scholastic Press",
         num_ratings: 6376780,
         liked_percent: 96,
-        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052.jpg",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546910265l/2.jpg",
         price: 5.09
     },
     {
         id:3,
-        title: "The Hunger Games",
-        author: "Suzanne Collins",
+        title: "Twilight",
+        author: "Stephenie Meyer",
         rating: 4.33,
         description: "WINNING MEANS FAME AND FORTUNE.LOSING MEANS CERTAIN DEATH.THE HUNGER GAMES HAVE BEGUN. . . .In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and once girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.Sixteen-year-old Katniss Everdeen regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love.",
         isbn: "9780439023481",
@@ -44,13 +44,13 @@ export const booksdata = [
         publisher: "Scholastic Press",
         num_ratings: 6376780,
         liked_percent: 96,
-        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052.jpg",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1361039443l/41865.jpg",
         price: 5.09
     },
     {
         id:4,
-        title: "The Hunger Games",
-        author: "Suzanne Collins",
+        title: "The Book Thief",
+        author: "Markus Zusak (Goodreads Author)",
         rating: 4.33,
         description: "WINNING MEANS FAME AND FORTUNE.LOSING MEANS CERTAIN DEATH.THE HUNGER GAMES HAVE BEGUN. . . .In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and once girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.Sixteen-year-old Katniss Everdeen regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love.",
         isbn: "9780439023481",
@@ -60,13 +60,13 @@ export const booksdata = [
         publisher: "Scholastic Press",
         num_ratings: 6376780,
         liked_percent: 96,
-        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052.jpg",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1522157426l/19063._SY475_.jpg",
         price: 5.09
     },
     {
         id:5,
-        title: "The Hunger Games",
-        author: "Suzanne Collins",
+        title: "Animal Farm",
+        author: "George Orwell, Russell Baker (Preface), C.M. Woodhouse (Introduction)",
         rating: 4.33,
         description: "WINNING MEANS FAME AND FORTUNE.LOSING MEANS CERTAIN DEATH.THE HUNGER GAMES HAVE BEGUN. . . .In the ruins of a place once known as North America lies the nation of Panem, a shining Capitol surrounded by twelve outlying districts. The Capitol is harsh and cruel and keeps the districts in line by forcing them all to send one boy and once girl between the ages of twelve and eighteen to participate in the annual Hunger Games, a fight to the death on live TV.Sixteen-year-old Katniss Everdeen regards it as a death sentence when she steps forward to take her sister's place in the Games. But Katniss has been close to dead before—and survival, for her, is second nature. Without really meaning to, she becomes a contender. But if she is to win, she will have to start making choices that weight survival against humanity and life against love.",
         isbn: "9780439023481",
@@ -76,7 +76,7 @@ export const booksdata = [
         publisher: "Scholastic Press",
         num_ratings: 6376780,
         liked_percent: 96,
-        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052.jpg",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1325861570l/170448.jpg",
         price: 5.09
     }
 ]
