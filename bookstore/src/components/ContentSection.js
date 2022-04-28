7a73b41cc12ddb31b22b794d346dcd5f2326d16c
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2022-04-28 20:44:37 +0530

    navbar update
    added dropdowns to the navbar items

--- bookstore/src/components/ContentSection.js
+++ bookstore/src/components/ContentSection.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
-import { Button, Card } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import BookCard from './BookCard';
 
 
 function ContentSection(props) {
@@ -13,17 +14,7 @@ function ContentSection(props) {
             <div className="book_sections_content" style={{ display: "flex", flexWrap: "wrap" }}>
                 {
                     props.data.slice(0, 4).map((book) => {
-                        console.log(book);
-                        return <Card style={{width: '13rem'}}>
-                            <div className="book_card_img">
-                                <Card.Img variant='top' src={book.image_url} style={{width:'100%',height:'100%',objectFit:'contain'}} />
-                            </div>
-                            <Card.Body>
-                                <Card.Title style={{fontSize:'16px',fontWeight:'bold'}}>{book.title}</Card.Title>
-                                <Card.Text style={{fontSize:'12px',margin:'3px 0'}}>{book.author}</Card.Text>
-                                <Card.Text style={{fontSize:'14px',fontWeight:'bold',color:'Highlight'}}>{"$ "+book.price}</Card.Text>
-                            </Card.Body>
-                        </Card>
+                        return <BookCard book={book}/>
                     })
                 }
             </div>
--- bookstore/src/components/Navbar.js
+++ bookstore/src/components/Navbar.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import { MdArrowDropDown } from "react-icons/md"
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className="navbar_content">
           <ul>
-            <li><a href="http://localhost:3000/">Genres</a></li>
-            <li><a href="http://localhost:3000/">Genres</a></li>
-            <li><a href="http://localhost:3000/">Genres</a></li>
-            <li><a href="http://localhost:3000/">Genres</a></li>
+            <li className='genre'><a href="/">Genres<i style={{fontSize:'21px'}}><MdArrowDropDown/></i></a>
+              <ul className='genre_dropdown'>
+                <li><a href="/">Action</a></li>
+                <li><a href="/">Adventure</a></li>
+                <li><a href="/">Classics</a></li>
+                <li><a href="/">Fantasy</a></li>
+                <li><a href="/">Fiction</a></li>
+                <li><a href="/">Literature</a></li>
+                <li><a href="/">Mystery</a></li>
+                <li><a href="/">Mythology</a></li>
+                <li><a href="/">Romance</a></li>
+                <li><a href="/">Politics</a> </li>
+              </ul>
+            </li>
+            <li><a href="http://localhost:3000/">New Arrivals</a></li>
+            <li><a href="http://localhost:3000/">Best Sellers</a></li>
+            <li><a href="http://localhost:3000/">Award Winners</a></li>
+            <li><a href="http://localhost:3000/">Contact</a></li>
           </ul>
         </div>
     </div>
