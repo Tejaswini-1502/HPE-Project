b7112d968a03730fa562ebc3ce31057cd09265b1
Tejaswini-1502 <tejaswiniv.is19@bmsce.ac.in>
2022-06-19 10:13:23 +0530

    Added Product Controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+import asyncHandler from 'express-async-handler'
+import Product from "../models/productModel.js";
+
+
+// @desc: fetch all  products
+// @route: GET /api/products
+// @access: Public
+
+const getProducts = asyncHandler(async (req,res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+// @desc: fetch a single product
+// @route: GET /api/product/:id
+// @access: Public
+
+const getProductById = asyncHandler(async (req,res) => {
+    const product = await Product.findById(req.params.id)
+     if(product){
+        res.json(product)
+     }else{
+        res.status(404)
+        throw new Error('Product not Found')
+     }
+})
+
+export {
+    getProducts,
+    getProductById
+}
