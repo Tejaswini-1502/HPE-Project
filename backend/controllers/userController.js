85756188d4fca9de7b800a95eb40bba20506d950
Tejaswini-1502 <tejaswiniv.is19@bmsce.ac.in>
2022-06-19 12:35:23 +0530

    added user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,32 @@
+import asyncHandler from 'express-async-handler'
+import User from "../models/userModel.js";
+
+
+// @desc: Auth the user & get a token
+// @route: POST /api/users/login
+// @access: Public
+
+const authUser = asyncHandler(async (req,res) => {
+    const { email, passsword } = req.body
+
+    const user = await User.findOne({ email })
+
+    if(user && (await user.matchPassword(passsword))){
+        res.json(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: null
+            }
+        )
+    }else{
+        res.status(401)
+        throw new Error('Invalid email or password')
+    }
+})
+
+export {
+    authUser
+}
