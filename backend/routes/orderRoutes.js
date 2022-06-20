c7ba743e6307d1dd1f771f04e7855bdc7fc73ee4
Tejaswini-1502 <tejaswiniv.is19@bmsce.ac.in>
2022-06-20 22:04:59 +0530

    Added order controller and route

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { addOrderItems } from "../controllers/orderController.js";
+const router = express.Router()
+
+
+router.route('/').post(addOrderItems)
+
+
+
+export default router
